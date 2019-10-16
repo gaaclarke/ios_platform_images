c5d80ebb005aca9ba42cd7749dc4802bfc846916
Aaron Clarke <aaclarke@google.com>
2019-10-16 13:13:54 -0700

    Created FutureMemoryImage to handle images with bytes not yet received.

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,7 +1,4 @@
 import 'package:flutter/material.dart';
-import 'dart:async';
-
-import 'package:flutter/services.dart';
 import 'package:native_images/native_images.dart';
 
 void main() => runApp(MyApp());
@@ -12,42 +9,9 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  ImageProvider _image;
-
   @override
   void initState() {
     super.initState();
-    initImage();
-  }
-
-  // Platform messages are asynchronous, so we initialize in an async method.
-  Future<void> initImage() async {
-    ImageProvider image;
-    // Platform messages may fail, so we use a try/catch PlatformException.
-    try {
-      image = await NativeImages.load("pug");
-    } on PlatformException {
-      print("unable to load image");
-    }
-
-    // If the widget was removed from the tree while the asynchronous platform
-    // message was in flight, we want to discard the reply rather than calling
-    // setState to update our non-existent appearance.
-    if (!mounted) return;
-
-    setState(() {
-      _image = image;
-    });
-  }
-
-  Widget makeChild() {
-    if (_image != null) {
-      return Image(
-        image: _image,
-      );
-    } else {
-      return Text("no image loaded");
-    }
   }
 
   @override
@@ -58,7 +22,7 @@ class _MyAppState extends State<MyApp> {
           title: const Text('Plugin example app'),
         ),
         body: Center(
-          child: makeChild(),
+          child: Image(image: NativeImages.load("pug")),
         ),
       ),
     );
--- lib/native_images.dart
+++ lib/native_images.dart
@@ -1,16 +1,64 @@
 import 'dart:async';
 import 'dart:typed_data';
+import 'dart:ui' as ui;
 
 import 'package:flutter/rendering.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter/painting.dart';
+import 'package:flutter/foundation.dart'
+    show SynchronousFuture, describeIdentity;
+
+// Performs exactly like a MemoryImage but instead of taking in bytes it takes
+// in a future that represents bytes.
+class FutureMemoryImage extends ImageProvider<FutureMemoryImage> {
+  const FutureMemoryImage(this.futureBytes, {this.scale = 1.0})
+      : assert(futureBytes != null),
+        assert(scale != null);
+
+  final Future<Uint8List> futureBytes;
+
+  final double scale;
+
+  @override
+  Future<FutureMemoryImage> obtainKey(ImageConfiguration configuration) {
+    return SynchronousFuture<FutureMemoryImage>(this);
+  }
+
+  @override
+  ImageStreamCompleter load(FutureMemoryImage key) {
+    return MultiFrameImageStreamCompleter(
+      codec: _loadAsync(key),
+      scale: key.scale,
+    );
+  }
+
+  Future<ui.Codec> _loadAsync(FutureMemoryImage key) async {
+    assert(key == this);
+    return futureBytes.then((Uint8List bytes) {
+      return PaintingBinding.instance.instantiateImageCodec(bytes);
+    });
+  }
+
+  @override
+  bool operator ==(dynamic other) {
+    if (other.runtimeType != runtimeType) return false;
+    final FutureMemoryImage typedOther = other;
+    return futureBytes == typedOther.futureBytes && scale == typedOther.scale;
+  }
+
+  @override
+  int get hashCode => hashValues(futureBytes.hashCode, scale);
+
+  @override
+  String toString() =>
+      '$runtimeType(${describeIdentity(futureBytes)}, scale: $scale)';
+}
 
 class NativeImages {
   static const MethodChannel _channel =
       const MethodChannel('plugins.flutter.io/native_images');
 
-  static Future<ImageProvider> load(String name) async {
-    final Uint8List data = await _channel.invokeMethod('loadImage', name);
-    return MemoryImage(data);
+  static FutureMemoryImage load(String name) {
+    return FutureMemoryImage(_channel.invokeMethod('loadImage', name));
   }
 }
